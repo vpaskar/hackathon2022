29b07cdf125f6e716a673dd5fc67dbfe0befd55e
Raymond <raymond.pinto@sap.com>
2022-07-20 16:38:02 +0200

    added first redirect from mainPage to editor

--- components/frontend/src/app/app/app.js
+++ components/frontend/src/app/app/app.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import KubeconfigReader from "../kubeconfigReader/kubeconfigReader";
 import Editor from "../editor/editor";
 import About from "../about/about";
+import {createBrowserHistory} from "history";
 
 function App() {
+    const history = createBrowserHistory();
     return (
         <div className="App">
             <Routes>
-                <Route path="/reader" element={<KubeconfigReader />} />
+                <Route path="/" element={<KubeconfigReader history={history}/>} />
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/about" element={<About />} />
             </Routes>
--- components/frontend/src/app/kubeconfigReader/kubeconfigReader.js
+++ components/frontend/src/app/kubeconfigReader/kubeconfigReader.js
@@ -1,11 +1,40 @@
+import React, { useState } from "react";
 import './kubeconfigReader.css';
+import {TextField} from "@mui/material";
+import {Button, Paper} from "@mui/material";
 
-function KubeconfigReader() {
+function KubeconfigReader(props) {
+  const [textValue, setTextValue] = useState(localStorage["kubeconfigPath"]);
+
+  const gotoEditor = () => {
+    props.history.push("/editor");
+    window.location.reload();
+  }
+
+  const onTextChange = e => setTextValue(e.target.value);
+  const handleSubmit = () => {
+    localStorage["kubeconfigPath"] = textValue;
+    // validate and if ok then redirect else show error
+    gotoEditor();
+  };
+  const handleReset = () => {
+    setTextValue("");
+    localStorage["kubeconfigPath"] = ""
+  }
   return (
     <div className="KubeconfigReader">
-      <header className="KubeconfigReader-header">
-        KubeconfigReader
-      </header>
+      <Paper>
+        <h2>Form Demo</h2>
+
+        <TextField
+          onChange={onTextChange}
+          value={textValue}
+          label={"Text Value"} //optional
+        />
+
+        <Button onClick={handleSubmit}>Submit</Button>
+        <Button onClick={handleReset}>Reset</Button>
+      </Paper>
     </div>
   );
 }
