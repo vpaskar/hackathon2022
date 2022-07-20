a7e55968fcfe4a04b9256ef97f5e4519503a80a1
Muhammad Faizan <m.faizan@sap.com>
2022-07-20 15:36:23 +0200

    added subscription client

--- components/backend/main.go
+++ components/backend/main.go
@@ -1,27 +1,60 @@
-package pochacki
+package main
 
 import (
-	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
-	eventingv1a1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
-	// xyz "function src"
+
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	subscription "github.com/vladislavpaskar/hackathon2022/components/backend/clients"
 )
 
-var Subscriptions []eventingv1a1.Subscription //crud
+
 //var Functions []xyz.Function //crud
 //logs //get
 //cloudevents //get
 
+var subscriptionClient subscription.Client
+
 func main() {
 	handleRequests()
+
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" && false {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", " /Users/faizan/kubeconfigs/kubeconfig--kymatunas--fw-hackihack.yml", "absolute path to the kubeconfig file")
+	}
+
+
+
+	flag.Parse()
+
+	k8sConfig, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create dynamic client (k8s)
+	dynamicClient := dynamic.NewForConfigOrDie(k8sConfig)
+
+	// setup clients
+	subscriptionClient = subscription.NewClient(dynamicClient)
+
+
+	log.Printf("Server listening ...")
 }
 
 func handleRequests() {
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("{ns}/subs/{name}", postSub).Methods("POST")
+	r.HandleFunc("/subs", getAllSubs).Methods("GET")
 	r.HandleFunc("{ns}/subs/{name}", getSub).Methods("GET")
 	r.HandleFunc("{ns}/subs/{name}", putSub).Methods("PUT")
 	r.HandleFunc("{ns}/subs/{name}", delSub).Methods("DELETE")
@@ -34,10 +67,23 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
-func subs(w http.ResponseWriter, r *http.Request) {
-	//GET all
-	for _, s := range Subscriptions {
-		json.NewEncoder(w).Encode(s)
+func getAllSubs(w http.ResponseWriter, r *http.Request) {
+
+	subsUnstructured, err := subscriptionClient.ListJson("default")
+	if err != nil {
+		log.Printf("%s %s failed: %v",r.Method, r.RequestURI , err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	subsBytes, err := subsUnstructured.MarshalJSON()
+	if err != nil {
+		log.Printf("%s %s failed to marchal json: %v",r.Method, r.RequestURI , err)
+	}
+
+	_, err = w.Write(subsBytes)
+	if err != nil {
+		log.Printf("%s %s failed to write response: %v",r.Method, r.RequestURI , err)
 	}
 }
 
