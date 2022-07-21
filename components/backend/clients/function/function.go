5ec016f9d6c6dbfea9aefcc0587fac4abab10e7b
Friedrich Wilken <jens.wilken@sap.com>
2022-07-21 14:12:30 +0200

    completed function endpoints
    fix typo

--- components/backend/clients/function/function.go
+++ components/backend/clients/function/function.go
@@ -2,6 +2,7 @@ package function
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	serverlessv1alpha1 "github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha1"
@@ -22,13 +23,13 @@ func NewClient(client dynamic.Interface) Client {
 }
 
 func (c Client) GetFnJson(name, namespace string) (*unstructured.Unstructured, error) {
-	subscriptionUnstructured, err := c.client.Resource(GroupVersionResource()).Namespace(namespace).Get(
+	functionUnstructured, err := c.client.Resource(GroupVersionResource()).Namespace(namespace).Get(
 		context.Background(), name, metav1.GetOptions{})
 
 	if err != nil {
 		return nil, err
 	}
-	return subscriptionUnstructured, nil
+	return functionUnstructured, nil
 }
 
 func (c Client) UpdateFunction(fn serverlessv1alpha1.Function) (*unstructured.Unstructured, error) {
@@ -46,8 +47,19 @@ func (c Client) UpdateFunction(fn serverlessv1alpha1.Function) (*unstructured.Un
 			return err
 		}
 
+		if err := unstructured.SetNestedField(result.Object, mapInterfaceFn["spec"], "spec"); err != nil {
+			return err
+		}
+
+		_, updateErr := c.client.Resource(GroupVersionResource()).Namespace(fn.Namespace).Update(context.Background(), result, metav1.UpdateOptions{})
+		return updateErr
 	})
 
+	if retryErr != nil {
+		return nil, err
+	}
+
+	return c.GetFnJson(fn.Name, fn.Namespace)
 }
 
 func GroupVersionResource() schema.GroupVersionResource {
@@ -56,4 +68,49 @@ func GroupVersionResource() schema.GroupVersionResource {
 		Group:    serverlessv1alpha1.GroupVersion.Group,
 		Resource: "function",
 	}
+}
+
+func (c Client) DeleteFunction(name, namespace string) error {
+	deletePolicy := metav1.DeletePropagationForeground
+	deleteOptions := metav1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	}
+	if err := c.client.Resource(GroupVersionResource()).Namespace(namespace).Delete(context.TODO(), name, deleteOptions); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c Client) List(namespace string) (*serverlessv1alpha1.FunctionList, error) {
+	functionUnstructured, err := c.client.Resource(GroupVersionResource()).Namespace(namespace).List(
+		context.Background(), metav1.ListOptions{})
+
+	if err != nil {
+		return nil, err
+	}
+	return toFunctionList(functionUnstructured)
+}
+
+func (c Client) ListJson(namespace string) (*unstructured.UnstructuredList, error) {
+	functionUnstructured, err := c.client.Resource(GroupVersionResource()).Namespace(namespace).List(
+		context.Background(), metav1.ListOptions{})
+
+	if err != nil {
+		return nil, err
+	}
+	return functionUnstructured, nil
+}
+
+func toFunctionList(unstructuredList *unstructured.UnstructuredList) (*serverlessv1alpha1.FunctionList, error) {
+	functionList := new(serverlessv1alpha1.FunctionList)
+	functionListBytes, err := unstructuredList.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(functionListBytes, functionList)
+	if err != nil {
+		return nil, err
+	}
+	return functionList, nil
 }
