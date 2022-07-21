430454f7a341dfa4399e3066b59e5701753ca38d
Raymond <raymond.pinto@sap.com>
2022-07-21 12:14:11 +0200

    Updated views

--- components/frontend/src/app/components/terminal.js
+++ components/frontend/src/app/components/terminal.js
@@ -1,16 +1,75 @@
 import React, {useState} from 'react';
 import Terminal, { ColorMode, LineType } from 'react-terminal-ui';
-import styles from "./overrides.css"
+import styles from "./overrides.css";
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
 
 const TerminalController = (props = {}) => {
   const [terminalLineData] = useState([
-    {type: LineType.Output, value: 'Logs from function:'},
     {type: LineType.Input, value: 'Log line 1'},
   ]);
+
+  const [terminalHeader, setTerminalHeader] = useState("Function Logs");
+
+  const handleChange = (event) => {
+    setTerminalHeader("Function logs from " + event.target.value);
+  };
   
   return (
     <div>
+        <Box sx={{
+          display: 'flex',
+          m: 1,
+          p: 1,
+          justifyContent: 'center',
+        }}>
+        <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'underline',
+              }}
+            >
+              Receive Events
+        </Typography>
+      </Box>
+    <Box sx={{
+          display: 'flex',
+          m: 1,
+          p: 1,
+          justifyContent: 'center',
+        }}>
+      <FormControl sx={{ m: 1, minWidth: 360}}>
+        <InputLabel id="demo-simple-select-helper-label">Function</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={"age"}
+          label="Function"
+          onChange={handleChange}
+        >
+          <MenuItem value={"Function1"}>Function1</MenuItem>
+          <MenuItem value={"Function2"}>Function2</MenuItem>
+          <MenuItem value={"Function3"}>Function3</MenuItem>
+        </Select>
+        <FormHelperText>Select Function Name</FormHelperText>
+      </FormControl>
+      </Box>
       <Terminal
+        name={terminalHeader}
         classname={styles}
         colorMode={ ColorMode.Dark }  
         lineData={ terminalLineData } />
--- components/frontend/src/app/kubeconfigReader/kubeconfigReader.js
+++ components/frontend/src/app/kubeconfigReader/kubeconfigReader.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import './kubeconfigReader.css';
 import {TextField} from "@mui/material";
-import {Button, Paper} from "@mui/material";
+import {Button, Box, Typography, Stack, FormControl} from "@mui/material";
+import ResponsiveAppBar from "../components/header";
 
 function KubeconfigReader(props) {
   const [textValue, setTextValue] = useState(localStorage["kubeconfigPath"]);
@@ -23,18 +24,58 @@ function KubeconfigReader(props) {
   }
   return (
     <div className="KubeconfigReader">
-      <Paper>
-        <h2>Form Demo</h2>
-
-        <TextField
+      <ResponsiveAppBar />
+      <Box sx={{
+          display: 'flex',
+          m: 1,
+          p: 1,
+          justifyContent: 'center',
+        }}>
+        <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'underline',
+              }}
+            >
+              Provide Kubeconfig
+        </Typography>
+      </Box>
+      <Box sx={{
+          display: 'flex',
+          m: 1,
+          p: 1,
+          justifyContent: 'center',
+        }}>
+          <FormControl sx={{ m: 1, minWidth: 420}}>
+      <TextField
+          id="outlined-multiline-static"
+          label="Kubeconfig content"
+          multiline
+          rows={10}
           onChange={onTextChange}
-          value={textValue}
-          label={"Text Value"} //optional
         />
-
-        <Button onClick={handleSubmit}>Submit</Button>
-        <Button onClick={handleReset}>Reset</Button>
-      </Paper>
+        </FormControl>
+      </Box>
+      <Box sx={{
+          display: 'flex',
+          m: 1,
+          p: 1,
+          justifyContent: 'center',
+        }}>
+        <Stack direction="row" spacing={2}>
+        <Button variant="contained" size="large" onClick={handleSubmit}>Submit</Button>
+        <Button variant="contained" size="large"  onClick={handleReset}>Reset</Button>
+        </Stack>
+      </Box>
     </div>
   );
 }
